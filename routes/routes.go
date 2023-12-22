7b65d3fe20e3cc6538b99f36f5d03d487a2f1bee
Jacob Saucelo <jacobsaucelo32@gmail.com>
2023-12-22 14:53:46 +0800

    Added port details

--- routes/routes.go
+++ routes/routes.go
@@ -1,14 +1,22 @@
 package routes
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 	"webGoCrud/controllers"
 )
+
+var portNumber uint16 = 3000
 
 func RoutesIndex() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", controllers.DisplayUsers)
 
-	log.Fatal(http.ListenAndServe(":3000", mux))
+	port := strconv.Itoa(int(portNumber))
+
+	fmt.Println("Server: Running on port", port)
+	log.Fatal(http.ListenAndServe(":"+port, mux))
+
 }
