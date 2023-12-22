919c41342f0a31fdc2483291851e308ba471c960
Jacob Saucelo <jacobsaucelo32@gmail.com>
2023-12-22 15:13:01 +0800

    added html table and form

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -15,9 +15,9 @@ var tmpl *template.Template
 func DisplayUsers(w http.ResponseWriter, r *http.Request) {
 	tmpl = template.Must(template.ParseFiles("views/index.html"))
 	page := utils.GetUsers()
+
 	fmt.Println("page: ", page)
 	tmpl.Execute(w, page)
-
 }
 
 // func (u *Users) AddUser(w http.ResponseWriter, r http.Request) {
